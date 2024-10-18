eb798edcc7e1645262e2e83e56331e0a3b292810
947AA <15681699283@163.com>
2024-10-18 14:50:53 +0800

    the commit is modified

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+
+	cm "github.com/947AA/grpc_chat/pb"
+	"google.golang.org/protobuf/proto"
 )
 
 func main() {
+
 	fmt.Println("Hello world!")
+	Com1 := cm.Cpu{}
+	data, _ := proto.Marshal(&Com1)
+	var sync []*cm.Cpu
+	com2 := cm.Cpu{}
+	proto.Unmarshal(data, &com2)
+
+	sync = append(sync, &com2)
+	fmt.Println(sync[0].String())
 }
